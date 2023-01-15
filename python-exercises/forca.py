c8937f39f24f30d26cfd8b7ce4ed4c0f27f7646e
adamcardoso <adamjuniorcardoso@gmail.com>
2023-01-15 02:24:47 -0300

    Starting the Forca game

--- python-exercises/forca.py
+++ python-exercises/forca.py
@@ -1,9 +1,17 @@
-
-
 def jogar():
     print("********************************")
     print("*********Jogo da Forca!*********")
     print("********************************")
+
+    palavra_secreta = "banana"
+
+    enforcou = False
+    acertou = False
+
+    while (not enforcou and not acertou):
+        print("jogando...")
+
+
 
     print("Fim do jogo")
 
