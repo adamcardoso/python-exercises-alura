0f5fbc1b4aea3396296707b6f96cea1c6d8d49bb
adamcardoso <adamjuniorcardoso@gmail.com>
2023-01-15 02:03:54 -0300

    updating the code and creating def functions

---
+++ python-exercises/forca.py
@@ -0,0 +1,11 @@
+
+
+def jogar():
+    print("********************************")
+    print("*********Jogo da Forca!*********")
+    print("********************************")
+
+    print("Fim do jogo")
+
+if (__name__ == "__main__"):
+    jogar()
--- python-exercises/games.py
+++ python-exercises/games.py
@@ -5,11 +5,15 @@ print("********************************")
 print("*********Escolha seu jogo!*******")
 print("********************************")
 
-print("(1) Forca (2) Adivinhação")
+print("(1) Forca (2) Adivinhação (3) Sair")
 
 jogo = int(input("Qual é o jogo? "))
 
 if (jogo == 1):
     print("Jogando Forca")
+    forca.jogar()
 elif (jogo == 2):
     print("Jogando Adivinhação")
+    jogo_de_adivinhacao.jogar()
+elif (jogo == 3):
+    print("Saindo...")
